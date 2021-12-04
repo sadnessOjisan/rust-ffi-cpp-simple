034ddd86e453a8cf251c6d4b8fe70ae85bf01bc4
sadnessOjisan <sadness.ojisan@gmail.com>
2021-12-05 05:31:42 +0900

    fix

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # rust-ffi-cpp-simple
+
 最小構成
 
 ## build
 
 ```
-gcc -fPIC -g -Wall -O2 -Wc++-compat -c ./src/test.c -o ./target/libtest.a
+gcc -fPIC -c ./src/test.c -o ./target/libtest.a
 ```
-
 
 ```
 cargo run
--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 use std::env;
 fn main(){
     let project_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    eprintln!("{}",project_dir);
     println!("cargo:rustc-link-search={}/target/", project_dir); // the "-L" flag
     println!("cargo:rustc-link-lib=test"); // the "-l" flag
 }
